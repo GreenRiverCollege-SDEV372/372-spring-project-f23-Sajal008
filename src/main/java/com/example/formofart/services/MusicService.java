98e5a1920212afb1b39d606e4646c587adbbe98e
Sajal <khadgi.sajal@student.greenriver.edu>
2024-03-04 23:35:58 -0800

    Logic corrected, bugs fixed

--- src/main/java/com/example/formofart/services/MusicService.java
+++ src/main/java/com/example/formofart/services/MusicService.java
@@ -1,5 +1,6 @@
 package com.example.formofart.services;
 
+import com.example.formofart.db.MusicRepository;
 import com.example.formofart.models.Music;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
@@ -7,39 +8,27 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Collections;
 
 @Service
-@SpringBootApplication
 public class MusicService {
-    private final List<Music> musicList = new ArrayList<>();
 
-    public List<Music> getAllMusic() {
-        return musicList;
+    private MusicRepository repository;
+
+    public MusicService(MusicRepository repository)
+    {
+        this.repository = repository;
     }
 
-    public Optional<Music> getMusicById(int id) {
-        return musicList.stream().filter(music -> music.getId() == id).findFirst();
+    public List<Music> all()
+    {
+        List<Music> movies = repository.findAll();
+        return Collections.unmodifiableList(movies);
     }
 
-    public Music createMusic(Music music) {
-        // Simulating auto-increment id
-        music.setId(musicList.size() + 1);
-        musicList.add(music);
-        return music;
-    }
-
-    public Music updateMusic(int id, Music updatedMusic) {
-        for (int i = 0; i < musicList.size(); i++) {
-            if (musicList.get(i).getId() == id) {
-                updatedMusic.setId(id);
-                musicList.set(i, updatedMusic);
-                return updatedMusic;
-            }
-        }
-        return null;
-    }
-
-    public boolean deleteMusic(int id) {
-        return musicList.removeIf(music -> music.getId() == id);
+    public Music addMusic(Music music)
+    {
+        //this will result in an INSERT SQL statement
+        return repository.save(music);
     }
 }
